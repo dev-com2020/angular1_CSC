1c2ee3640245e99109393db615098b10cc790f1c
DEV-COM <kwp@tlen.pl>
2022-03-17 11:42:16 +0100

    czwartek 11:40

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -19,20 +19,22 @@ export class FormComponent {
     product: Product = new Product(0);
     // lastId: number;
 
-    constructor(private model: Model, activeRoute: ActivatedRoute, private router: Router){
+    constructor(public model: Model, activeRoute: ActivatedRoute, private router: Router){
         this.editing = activeRoute.snapshot.params["mode"] == "edit";
-        let id = activeRoute.snapshot.params["id"];
-        if (id != undefined){
-            let name = activeRoute.snapshot.params["name"];
-            let category = activeRoute.snapshot.params["category"];
-            let price = activeRoute.snapshot.params["price"];
 
-        if (name != undefined && category !=undefined && price !=undefined){
-            this.product.id = id;
-            this.product.name = name;
-            this.product.category = category;
-            this.product.price = Number.parseFloat(price);
-        }
+        let id = activeRoute.snapshot.params["id"];
+        
+        if (id != undefined){
+        //     let name = activeRoute.snapshot.params["name"];
+        //     let category = activeRoute.snapshot.params["category"];
+        //     let price = activeRoute.snapshot.params["price"];
+
+        // if (name != undefined && category !=undefined && price !=undefined){
+        //     this.product.id = id;
+        //     this.product.name = name;
+        //     this.product.category = category;
+        //     this.product.price = Number.parseFloat(price);
+        // }
             Object.assign(this.product, model.getProduct(id) || new Product(0));
         }
     }
